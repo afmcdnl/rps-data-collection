6771f731b671083f0b4a222e9cd19fd32f229e42
Andrew <afmcdnl@gmail.com>
2021-02-23 10:49:17 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,48 @@
+let Coin = 0
+let item = 0
+let Rock = 0
+/**
+ * Shake
+ * 
+ * Pick a random number
+ * 
+ * If number is 0 -> Tail
+ * 
+ * if number is 1 Heads
+ */
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Andrew")
+})
+input.onButtonPressed(Button.B, function () {
+    Coin = randint(0, 1)
+    if (Coin == 0) {
+        basic.showLeds(`
+            # # # # #
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+    } else if (Coin == 1) {
+        basic.showLeds(`
+            # . . . #
+            # . . . #
+            # # # # #
+            # . . . #
+            # . . . #
+            `)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 1000; index++) {
+        item = randint(0, 2)
+        if (item == 0) {
+            basic.showIcon(IconNames.SmallSquare)
+            Rock += 1
+        } else if (item == 1) {
+            basic.showIcon(IconNames.Square)
+        } else {
+            basic.showIcon(IconNames.Scissors)
+        }
+    }
+})
